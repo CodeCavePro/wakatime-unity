ca0784096981182991e1494ba5c29aeb9abc1a7a
Zhmayev Yaroslav <zhmayev.yaroslav@codecave.pro>
2016-08-21 20:38:36 +0300

    fixing target .NET framework version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes. 
+// Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("WakaTime.Unity")]
-[assembly: AssemblyDescription("WakaTime is a productivity & time tracking tool for programmers. Once the WakaTime plugin is installed, you get a dashboard with reports about your programming by time, language, project, and branch.")]
-[assembly: AssemblyProduct("WakaTime Unity3d plugin")]
+[assembly: AssemblyDescription("WakaTime is a productivity & time tracking tool for programmers. Once the WakaTime plug-in is installed, you get a dashboard with reports about your programming by time, language, project, and branch.")]
+[assembly: AssemblyProduct("WakaTime Unity3d plug-in")]
 [assembly: AssemblyCopyright("Copyright © José Manuel Cámara Iglesias (josec89) 2015")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("1.0.*")]
 
-// The following attributes are used to specify the signing key for the assembly, 
+// The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
